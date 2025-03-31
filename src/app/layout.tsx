c51796c590971ef7b52ccd9e2a09dd7c222de090
Marcy <marcelly.nascimento84@gmail.com>
2025-03-31 11:01:11 -0300

    adicionando favicon atraves do openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
@@ -14,6 +13,27 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Salão Rosa de Saron",
   description: "O melhor Salão de beleza e estética da Praia Grande",
+  openGraph: {
+    title: "Salão Rosa de Saron",
+    description: "O melhor Salão de beleza e estética da Praia Grande",
+    url: "https://www.salaorosadesaronpg.com.br/", // Substitua pela URL real do seu site
+    siteName: "Salão Rosa de Saron",
+    images: [
+      {
+        url: "./public/icon.ico", // Substitua pela URL real da imagem
+        width: 1200,
+        height: 630,
+        alt: "Salão Rosa de Saron",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Salão Rosa de Saron",
+    description: "O melhor Salão de beleza e estética da Praia Grande",
+    images: ["./public/icon.ico"], // URL da imagem para Twitter
+  },
 };
 
 export default function RootLayout({
